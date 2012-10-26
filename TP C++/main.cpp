33fec60d98f2f654941c58aaf8e2c2e3471fd5b8
garronde <garrondeau@gmail.com>
2012-10-26 14:13:25 +0200

    Instal

---
+++ TP C++/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  TP C++
+//
+//  Created by Gaëtan Arrondeau on 26/10/12.
+//  Copyright (c) 2012 Gaëtan Arrondeau. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+a
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
